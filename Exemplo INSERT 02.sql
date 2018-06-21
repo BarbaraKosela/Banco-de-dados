569b7c92575439ad8b26c63367506185188c562a
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-21 14:30:56 -0300

    Exemplo com mangás;

---
+++ Exemplo INSERT 02.sql
@@ -0,0 +1,9 @@
+﻿
+
+
+SELECT * FROM mangas;
+
+INSERT INTO mangas VALUES ('Naruto', 67, 2015);
+INSERT INTO mangas VALUES ('Bleach', 70, 2015);
+INSERT INTO mangas VALUES ('Nanatsu', 20, 2015);
+INSERT INTO mangas VALUES ('Dragon Ball',8001, 2004);
