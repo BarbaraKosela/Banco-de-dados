c070723574c152c6907d7bc516f79c03a7722331
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-21 16:27:25 -0300

    Primeiro exercício da lista, cadastro de pessoas

--- Exemplo INSERT 02.sql
+++
@@ -1,9 +0,0 @@
-﻿
-
-
-SELECT * FROM mangas;
-
-INSERT INTO mangas VALUES ('Naruto', 67, 2015);
-INSERT INTO mangas VALUES ('Bleach', 70, 2015);
-INSERT INTO mangas VALUES ('Nanatsu', 20, 2015);
-INSERT INTO mangas VALUES ('Dragon Ball',8001, 2004);
