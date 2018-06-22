be3a3e19219285bbf5b1b6d23b7f44b2dd01f463
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-22 17:08:16 -0300

    New Example

---
+++ Exemplo LIKE.sql
@@ -0,0 +1,21 @@
+﻿DROP TABLE produtos;
+CREATE TABLE produtos(
+	nome VARCHAR(100)
+
+);
+
+INSERT INTO produtos VALUES
+('Calabresa'),
+('X-Calabresa'),
+('Pizza de calabresa'),
+('Calabresa com miojo'),
+('Milkshake de calabresa'),
+('Paçoca de calabresa com açaí'),
+('Polenta com paçoca');
+
+-- LIKE SERVE PARA BUSCAR OS PRODUTOS QUE CONTÉM O NOME INICIANDO COM CALABRESA
+SELECT * FROM produtos WHERE nome LIKE 'Calabresa%';
+-- QUE TERMINAM COM CALABRESA
+SELECT * FROM produtos WHERE nome LIKE '%Calabresa';
+-- BUSCAR CALABRESA EM QUALQUER PARTE
+SELECT * FROM produtos WHERE nome LIKE '%Calabresa%';
