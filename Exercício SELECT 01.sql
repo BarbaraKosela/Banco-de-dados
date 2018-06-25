d9325c94cd50ff30e049ed752e61eb721de23fef
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-25 16:32:32 -0300

    Exercício de número 3 - alunos

--- Exercício SELECT 01.sql
+++ Exercício SELECT 01.sql
@@ -1,44 +1,62 @@
-﻿-- EXERCÍCIO 01 : SELECT id 'ID', nome 'nome', codigo 'codigo', categoria 'categoria', altura 'altura', peso 'peso', hp 'hp', ataque 'ataque', defesa 'defesa', especial_ataque 'ataque especial', especial_defesa 'defesa especial', velocidade 'velocidade', descricao 'descrição' FROM pokemons;
+﻿	
+	-- EXERCÍCIO 01 : SELECT id 'ID', nome 'nome', codigo 'codigo', categoria 'categoria', altura 'altura', peso 'peso', hp 'hp', ataque 'ataque', defesa 'defesa', especial_ataque 'ataque especial', especial_defesa 'defesa especial', velocidade 'velocidade', descricao 'descrição' FROM pokemons;
 
--- EXERCÍCIO 02 : SELECT nome 'nome', ataque 'ataque', especial_ataque 'ataque especial' , defesa 'defesa', especial_defesa 'defesa especial' FROM pokemons;
+	-- EXERCÍCIO 02 : SELECT nome 'nome', ataque 'ataque', especial_ataque 'ataque especial' , defesa 'defesa', especial_defesa 'defesa especial' FROM pokemons;
 
--- EXERCÍCIO 03 : SELECT nome 'nome', categoria 'categoria', ataque 'ataque' FROM pokemons ORDER BY ataque ASC;
+	-- EXERCÍCIO 03 : SELECT nome 'nome', categoria 'categoria', ataque 'ataque' FROM pokemons ORDER BY ataque ASC;
 
--- EXERCÍCIO 04 : SELECT nome 'nome', peso 'peso', altura 'altura',  peso / (altura * altura) 'IMC' FROM pokemons;
+	-- EXERCÍCIO 04 : SELECT nome 'nome', peso 'peso', altura 'altura',  peso / (altura * altura) 'IMC' FROM pokemons;
 
--- EXERCÍCIO 05 : SELECT nome 'nome', peso 'peso', altura 'altura',  peso / (altura * altura) 'IMC' FROM pokemons ORDER BY peso / (altura * altura) DESC;
+	-- EXERCÍCIO 05 : SELECT nome 'nome', peso 'peso', altura 'altura',  peso / (altura * altura) 'IMC' FROM pokemons ORDER BY peso / (altura * altura) DESC;
 
--- EXERCÍCIO 06 : SELECT nome 'nome', LEN(nome) 'QUANTIDADE DE CARACTERES' FROM pokemons WHERE LEN(nome) > 1 ORDER BY LEN(nome) DESC;
+	-- EXERCÍCIO 06 : SELECT nome 'nome', LEN(nome) 'QUANTIDADE DE CARACTERES' FROM pokemons WHERE LEN(nome) > 1 ORDER BY LEN(nome) DESC;
 
--- EXERCÍCIO 07 : SELECT nome 'nome',  descricao 'descrição' FROM pokemons WHERE LEN(nome) > 10;
+	-- EXERCÍCIO 07 : SELECT nome 'nome',  descricao 'descrição' FROM pokemons WHERE LEN(nome) > 10;
 
--- EXERCÍCIO 08 : SELECT nome 'nome', categoria 'categoria', ataque 'ataque' FROM pokemons WHERE  ataque = (SELECT MIN(ataque) FROM pokemons);
+	-- EXERCÍCIO 08 : SELECT nome 'nome', categoria 'categoria', ataque 'ataque' FROM pokemons WHERE  ataque = (SELECT MIN(ataque) FROM pokemons);
 
--- EXERCÍCIO 09 : SELECT nome 'nome' ,ataque 'ataque', especial_ataque 'ataque especial', defesa 'defesa', especial_defesa 'defesa especial' FROM pokemons ORDER BY ataque;
+	-- EXERCÍCIO 09 : SELECT nome 'nome' ,ataque 'ataque', especial_ataque 'ataque especial', defesa 'defesa', especial_defesa 'defesa especial' FROM pokemons ORDER BY ataque;
 
--- SELECT AVG(ataque) 'Média de ataques' FROM pokemons;
+	-- SELECT AVG(ataque) 'Média de ataques' FROM pokemons;
 
--- EXERCÍCIO 11 : SELECT SUM(ataque) 'Soma dos ataques' FROM pokemons;
+	-- EXERCÍCIO 11 : SELECT SUM(ataque) 'Soma dos ataques' FROM pokemons;
 
--- EXERCÍCIO 12 : SELECT AVG(especial_ataque) 'Média ataque especial' FROM pokemons WHERE nome LIKE 'P%';
+	-- EXERCÍCIO 12 : SELECT AVG(especial_ataque) 'Média ataque especial' FROM pokemons WHERE nome LIKE 'P%';
 
 
--- PARTE 2 -- 
+                                                                            -- PARTE 2 -- 
 
--- EXERCÍCIO 01 : SELECT estado 'estado', cidade 'cidade' FROM cidadezinhas;
 
--- EXERCÍCIO 02 : SELECT * FROM cidades WHERE cidade LIKE 'A%';
+	-- EXERCÍCIO 01 : SELECT estado 'estado', cidade 'cidade' FROM cidadezinhas;
 
--- EXERCÍCIO 03 : SELECT * FROM cidades WHERE cidade LIKE '%Apar%';
+	-- EXERCÍCIO 02 : SELECT * FROM cidades WHERE cidade LIKE 'A%';
 
--- EXERCÍCIO 04 : SELECT * FROM cidades WHERE cidade LIKE 'W%';
+	-- EXERCÍCIO 03 : SELECT * FROM cidades WHERE cidade LIKE '%Apar%';
 
--- EXERCÍCIO 05 : SELECT * FROM cidades WHERE cidade LIKE '%tuba' ORDER BY estado DESC;
+	-- EXERCÍCIO 04 : SELECT * FROM cidades WHERE cidade LIKE 'W%';
 
--- EXERCÍCIO 06 : SELECT cidade, LEN(cidade) 'QUANTIDADE DE CARACTERES DAS CIDADES' FROM cidades WHERE LEN(cidade) > 15 ORDER BY LEN(cidade);
+	-- EXERCÍCIO 05 : SELECT * FROM cidades WHERE cidade LIKE '%tuba' ORDER BY estado DESC;
 
--- EXERCÍCIO 07 : SELECT COUNT(cidade) AS 'QUANTIDADE DE CIDADES COM SC' FROM cidades WHERE estado LIKE '%SC';
+	-- EXERCÍCIO 06 : SELECT cidade, LEN(cidade) 'QUANTIDADE DE CARACTERES DAS CIDADES' FROM cidades WHERE LEN(cidade) > 15 ORDER BY LEN(cidade);
 
--- EXERCÍCIO 08 : SELECT COUNT(cidade) AS 'QUANTIDADE DE CIDADES COM SP' FROM cidades WHERE estado LIKE '%SP';
+	-- EXERCÍCIO 07 : SELECT COUNT(cidade) AS 'QUANTIDADE DE CIDADES COM SC' FROM cidades WHERE estado LIKE '%SC';
 
--- EXERCÍCIO 09 : SELECT cidade, LEN(cidade) 'QUANTIDADE DE CARACTERES' FROM cidades WHERE LEN(cidade) > 10 ORDER BY LEN(cidade);
+	-- EXERCÍCIO 08 : SELECT COUNT(cidade) AS 'QUANTIDADE DE CIDADES COM SP' FROM cidades WHERE estado LIKE '%SP';
+
+	-- EXERCÍCIO 09 : SELECT cidade, LEN(cidade) 'QUANTIDADE DE CARACTERES' FROM cidades WHERE LEN(cidade) > 10 ORDER BY LEN(cidade);
+
+	
+
+	                                                                            -- PARTE 3 -- 
+
+	-- EXERCÍCIO 01 : SELECT nome 'Nome', cpf 'CPF', nick 'Nickname', signo 'Signo', numero_favorito 'Número favorito',nota_1 'Nota 1', nota_2 'Nota 2', nota_3 'Nota 3', nota_4 'Nota 4', cor_preferida 'Cor preferida', data_nascimento 'Data de nascimento' FROM alunoos;
+
+	-- EXERCÍCIO 02 : SELECT nome, nota_1 'nota 1' FROM alunoos WHERE nota_1 > 9;
+
+    -- EXERCÍCIO 03 : SELECT nome 'Nome', nota_1 'Nota 1', nota_2 'Nota 2', nota_3 'Nota 3', nota_4 'Nota 4', (nota_1 + nota_2 + nota_3 + nota_4) /4 'Média' FROM alunoos;
+
+
+
+	
+
+	
