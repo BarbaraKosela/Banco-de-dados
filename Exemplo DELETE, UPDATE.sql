ccf273ffecc26a07495c994647b4904dd631b2d8
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-26 14:21:42 -0300

    Delete Update

---
+++ Exemplo DELETE, UPDATE.sql
@@ -0,0 +1,15 @@
+﻿SELECT * FROM outfits;
+
+UPDATE outfits SET tamanho = 'XGG' WHERE marca = 'Lalis Blank';
+UPDATE outfits SET marca = 'Lelis Blanc' WHERE marca = 'Lalis Blank';
+UPDATE outfits SET cor = 'Rosa choque' WHERE marca = 'Lelis Blanc';
+UPDATE outfits SET tecido = 'ceda', valor = 1.50 WHERE marca = 'Lelis Blanc';
+
+
+UPDATE outfits SET marca = 'Oakley' WHERE marca = 'Okle';
+
+
+UPDATE outfits SET cor = 'Azul espacial' WHERE tecido LIKE '%Malha%';
+
+DELETE FROM outfits WHERE cor = 'Azul espacial' -- OR cor 'Verde';
+DELETE FROM outfits WHERE cor != 'Azul espacial';
---
+++ INSERT OUTFIT LIKE.sql
@@ -0,0 +1,25 @@
+﻿	CREATE TABLE outfits(
+		
+		marca VARCHAR(100),
+		valor DECIMAL(8,2),
+		tamanho CHAR(3),
+		cor VARCHAR(20),
+		tecido VARCHAR(40)
+
+	);
+
+	INSERT INTO outfits VALUES('Supreme', 700, 'M', 'Cinza espacial', 'TNT'),
+    ('Gucci', 15000, 'P', 'Preto com vermelho', 'Laicra'), 
+	('Lalis Blank', 15 , '', 'Rosa xoque', 'Seda'), 
+	('Nike', 400, 'M', 'Indefinida', 'Malha'), 
+	('Gucci',2500 , 'M', 'Verde', '150% malha'), 
+	('Okle', 500, 'PP', 'Fogo', 'Lona'), 
+	('Chili bian', 14999, 'GGG', 'Branco', 'Diamente');
+
+
+
+
+
+
+
+
