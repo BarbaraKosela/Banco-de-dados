296b92157ec7388903328e4c7856c7ff03d0d282
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-28 15:58:33 -0300

    Exercício 01 com problemas(ajeitar)

--- Exercício UPDATE.sql
+++ Exercício UPDATE.sql
@@ -1,7 +1,7 @@
 ﻿SELECT * FROM pokemons;
 
 --  EXERCÍCIO 1 : código estiver no intervalo de 50 a 100
-UPDATE pokemons SET categoria = 'Seed' WHERE codigo = 50 > AND codigo < 100;
+UPDATE pokemons SET categoria = 'Seed' WHERE codigo > 50  AND codigo < 100;
 
 --  EXERCÍCIO 2 : nome conter ‘inj’ = ataque 29
 UPDATE pokemons SET ataque = 29 WHERE nome LIKE '%inj%';
@@ -18,8 +18,7 @@ UPDATE pokemons SET nome = 'C%' WHERE nome LIKE 'R%';
 --  EXERCÍCIO 6 : altura for 0.5 altura 0.51 peso 0.70
 UPDATE pokemons SET altura = 0.51, peso = 0.70 WHERE altura = 0.5; 
 
---  EXERCÍCIO 7 : defesa especial for 3 e ataque especial for 4 código 1, defesa 1
-ataque 1, ataque especial 3, defesa especial 4 
+--  EXERCÍCIO 7 : defesa especial for 3 e ataque especial for 4 código 1, defesa 1 ataque 1, ataque especial 3, defesa especial 4 
 UPDATE pokemons SET codigo = 1, defesa = 1, ataque =1, especial_ataque =3, especial_defesa = 4 WHERE especial_defesa = 3 AND especial_ataque = 4;
 
 --  EXERCÍCIO 8 : nome contiver mais 10 caracteres nome obter somente os dez caracteres do nome
@@ -108,6 +107,5 @@ UPDATE alunoos SET data_nascimento = CONVERT(CHAR(4), DATEPART(yyyy, data_nascim
 -- EXERCÍCIO 10 : cor preferida for roxo ou coral cor preferida roxo categoria Roxolandia 
 UPDATE alunoos SET cor_preferida = 'Roxo' WHERE cor_preferida = 'Roxo' OR cor_preferida = 'Coral';
 
--- EXERCÍCIO 11 : data de nascimento o mês for julho data nascimento trocar o mês para junho
-data nascimento trocar o ano para 2018
+-- EXERCÍCIO 11 : data de nascimento o mês for julho data nascimento trocar o mês para junho data nascimento trocar o ano para 2018
 UPDATE alunoos SET data_nascimento = 2018-06 WHERE MONTH(data_nascimento) = 07;
